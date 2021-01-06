71b6cc7786f4a994b0c9bb8b00d29725e40b7c49
Omri Zohar <omriz@google.com>
2021-01-06 19:53:45 +0000

    Temporary - creating a pdf

--- go.mod
+++ go.mod
@@ -4,5 +4,15 @@ go 1.15
 
 require (
 	cloud.google.com/go v0.74.0
+	github.com/TV4/graceful v0.3.4 // indirect
+	github.com/go-akka/configuration v0.0.0-20200606091224-a002c0330665 // indirect
+	github.com/gogap/config v0.0.0-20190801085516-e664631840ac
+	github.com/gogap/go-pandoc v0.0.0-20200716062510-7550ed141704
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/pborman/uuid v1.2.1 // indirect
+	github.com/phyber/negroni-gzip v1.0.0 // indirect
+	github.com/rs/cors v1.7.0 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
 	google.golang.org/genproto v0.0.0-20201214200347-8c77b98c765d
 )
--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"context"
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 
 	vision "cloud.google.com/go/vision/apiv1"
+	"github.com/gogap/config"
+	"github.com/gogap/go-pandoc/pandoc"
+	_ "github.com/gogap/go-pandoc/pandoc/fetcher/data"
 )
 
+var pandocConf *config.Config
+
 func main() {
+	pandocConf = config.NewConfig(
+		config.ConfigFile("pandoc.conf"),
+	)
 	fmt.Println("Starting Server")
 	http.HandleFunc("/", HandleOCR)
 	http.ListenAndServe(":8080", nil)
@@ -38,6 +48,22 @@ func parseBody(ctx context.Context, b io.ReadCloser) (string, error) {
 	return annotations[0].Description, nil
 }
 
+func parseMarkdown(ctx context.Context, md string) ([]byte, error) {
+	pdoc, err := pandoc.New(pandocConf)
+	if err != nil {
+		return nil, err
+	}
+	fetcherOpts := pandoc.FetcherOptions{
+		Name:   "data",
+		Params: json.RawMessage(`{"data": "` + string(base64.StdEncoding.EncodeToString([]byte(md))+`"}`)),
+	}
+	convertOpts := pandoc.ConvertOptions{
+		From: "markdown",
+		To:   "pdf",
+	}
+	return pdoc.Convert(fetcherOpts, convertOpts)
+}
+
 // HandleOCR handles image requests
 func HandleOCR(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -53,5 +79,11 @@ func HandleOCR(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte(fmt.Sprintf("Error %v", err)))
 		return
 	}
-	fmt.Fprint(w, ss)
+	d, err := parseMarkdown(ctx, ss)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Error %v", err)))
+		return
+	}
+	w.Write(d)
 }
